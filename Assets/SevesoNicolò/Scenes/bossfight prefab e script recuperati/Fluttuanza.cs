9a9251ce51c3b82fcaea00f350df9eb318bea7ef
Daniele Sangineto <scidros@gmail.com>
2017-01-26 22:01:02 +0100

    - recuperata la scena del terro (BOSSFIGHT) - recuperata la cartella "PlaceHolder Da Internet" contentente gran parte degli oggetti presenti in scena - creata una cartella chiamata Fluttuanze nella cartella Nicolo/Scene in cui ho raccolto un po di materiale (script della fluttuanze e prefab della scena BOSSFIGHT)

---
+++ Assets/SevesoNicolò/Scenes/bossfight prefab e script recuperati/Fluttuanza.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fluttuanza : MonoBehaviour {
+    public GameObject a;
+    public GameObject b;
+    public float n;
+    private Vector3 startPos, endPos;
+
+    
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        startPos = this.transform.position;
+        endPos = startPos + new Vector3(0, 20, 0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = Vector3.Lerp(startPos, endPos, Mathf.PerlinNoise(Time.time, Time.time)/5);
+
+
+	}
+}
