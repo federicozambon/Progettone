6b568eff59852c9ddaa52e7c5a5c228f9e553053
Zambon Federico <zambon.fe@gmail.com>
2017-01-31 03:59:53 +0100

    no message

--- Assets/PROTOTIPO/Script/MovingObjects/WallMover.cs
+++ Assets/PROTOTIPO/Script/MovingObjects/WallMover.cs
@@ -4,32 +4,4 @@ using System.Collections.Generic;
 
 public class WallMover: MonoBehaviour
 {
-    WaveController wcRef;
-    Vector3 startPos;
-    Vector3 endPos;
-    Vector3 currentVelocity;
-
-    float timer;
-
-    void Awake()
-    {
-        wcRef = FindObjectOfType<WaveController>();
-    }
-
-    void Start()
-    {
-        startPos = this.transform.localPosition;
-        endPos = startPos - new Vector3(0,1,0);
-        wcRef.firstDoorsList.Add(this);
-    }
-
-    public IEnumerator HideWall ()
-    {
-        while (endPos.y < transform.localPosition.y-0.2f)
-        {
-            timer += Time.deltaTime;
-            this.transform.localPosition = Vector3.Lerp(startPos, endPos, timer/2);
-            yield return null;
-        }
-    }
 }
