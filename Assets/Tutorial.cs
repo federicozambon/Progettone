4c81f9e4996900be958d03309906fbe0c020a003
Zambon Federico <zambon.fe@gmail.com>
2017-01-31 01:55:30 +0100

    no message

--- Assets/Tutorial.cs
+++ Assets/Tutorial.cs
@@ -2,11 +2,11 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class Tutorial : MonoBehaviour {
-
+public class Tutorial : MonoBehaviour
+{
     Player player;
     FlyCamManager flyElements;
-    int step = 0;
+    public int step = 0;
     public GameObject nemico1;
     public GameObject nemico2;
     public GameObject nemico3;
@@ -26,10 +26,7 @@ public class Tutorial : MonoBehaviour {
             player.dashAttivo = false;
             player.noWeapons = true;
             player.tutorialMode = true;
-        }
-
-        
-        
+        }     
 	}
 
     void Start()
@@ -37,7 +34,7 @@ public class Tutorial : MonoBehaviour {
         if (currentScene == "Tutorial")
         {
             NextStep();
-            flyElements.Skip();
+            //flyElements.Skip();
         }
     }
 	
@@ -53,8 +50,7 @@ public class Tutorial : MonoBehaviour {
                 transform.GetChild(1).gameObject.SetActive(true);
                 break;
             case 3:
-                transform.GetChild(2).gameObject.SetActive(true);
-                
+                transform.GetChild(2).gameObject.SetActive(true);             
                 break;
             case 4:
                 transform.GetChild(3).gameObject.SetActive(true);
@@ -99,8 +95,6 @@ public class Tutorial : MonoBehaviour {
     {
         if (currentScene == "Tutorial")
         {
-            
-
             if (Input.GetButtonDown("Fire1") && step == 1)
             {
                 HideStep();
@@ -111,6 +105,7 @@ public class Tutorial : MonoBehaviour {
             {
                 HideStep();
                 player.dashAttivo = true;
+                NextStep();
             }
             if (Input.GetButtonDown("Fire1") && step == 3)
             {
@@ -118,44 +113,33 @@ public class Tutorial : MonoBehaviour {
                 player.noWeapons = false;
                 nemico1.gameObject.SetActive(true);
             }
-
-
             if (Input.GetButtonDown("Next Weapon") && step == 4)
             {
                 HideStep();
-
                 nemico2.gameObject.SetActive(true);
             }
-
             if (Input.GetButtonDown("Previous Weapon") && step == 5)
             {
                 HideStep();
-
                 nemico3.gameObject.SetActive(true);
             }
-
             if (step == 3 && nemico1.gameObject == null)
             {
                 NextStep();
             }
-
             if (step == 4 && nemico2.gameObject == null)
             {
                 NextStep();
             }
-
             if (step == 5 && nemico3.gameObject == null)
             {
                 NextStep();
             }
-
             if (Input.GetButtonDown("Fire1") && step == 6)
             {
                 HideStep();
-
                 int indexSC = SceneManager.GetActiveScene().buildIndex;
                 SceneManager.LoadScene(indexSC + 1);
-
             }
         }
     }
