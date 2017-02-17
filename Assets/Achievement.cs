b2f06ff0de28ceab61bd01c567bc7bda44ef1e28
dbga_26 <dbga_26@dbga.local>
2017-02-17 16:12:25 +0100

    - base sistema di achievement

---
+++ Assets/Achievement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Achievement : MonoBehaviour {
+
+    public int total = 0;
+    public int tutorial = 0;
+    public int montacarichiA = 0;
+    public int discarica = 0;
+    public int montacarichiB = 0;
+    public int tetto = 0;
+	
+	void Start () {
+
+        DontDestroyOnLoad(this.gameObject);
+	}
+	
+	
+	void SaveScore () {
+	
+	}
+}
