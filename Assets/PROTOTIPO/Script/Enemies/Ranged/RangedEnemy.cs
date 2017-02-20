dc55bc0e48d411861f4cf8d53d6d97a83ec13dff
dbga_29 <dbga_29@dbga.local>
2017-02-20 18:19:55 +0100

    start implementation

--- Assets/PROTOTIPO/Script/Enemies/Ranged/RangedEnemy.cs
+++ Assets/PROTOTIPO/Script/Enemies/Ranged/RangedEnemy.cs
@@ -6,9 +6,11 @@ public class RangedEnemy : Enemy
 {
     public GameObject particlePoolPrefab;
     public RangedEnemyFire rangedFireRef;
+    Animator animRef;
 
     void Start()
     {
+        animRef = GetComponent<Animator>();
         rangedFireRef = GetComponent<RangedEnemyFire>();
         hPoints = 25;
         comboValue = 10;
@@ -35,6 +37,19 @@ public class RangedEnemy : Enemy
 
     void Update()
     {
+
+        if (!rangedFireRef.isShooting)
+        {
+            animRef.SetFloat("Blend", 0);
+            animRef.SetBool("Attack", true);
+        }
+        else
+        {
+            animRef.SetFloat("Blend", Mathf.Clamp01(navRef.velocity.magnitude));
+            animRef.SetBool("Attack", false);
+        }
+        Debug.LogWarning(Mathf.Clamp01(navRef.velocity.magnitude));
+
         transform.LookAt(new Vector3(refManager.playerObj.transform.position.x, this.transform.position.y, refManager.playerObj.transform.position.z));
     }
 }
