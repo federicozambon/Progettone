c4e1de2d5dc70459d1761c53caeee8efff60e1b0
dbga_15 <dbga_15@dbga.local>
2017-02-20 18:06:57 +0100

    Canvas

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
     [Range(0, 1)]
     public float audiovolume;
 
-
+   
     public void Update ()
     {
         AudioListener.volume = audiovolume;
+    }
+    public void QuitToMenu ()
+    {
+        
     }
 }
--- Assets/PauseManager.cs
+++ Assets/PauseManager.cs
@@ -1,15 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class PauseManager : MonoBehaviour {
+    public GameObject CanvasPause;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Time.timeScale = 0;
+            CanvasPause.SetActive(true);
+        }    
+    }
+    public void QuitToMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Menu");
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        CanvasPause.SetActive(false);
+    }
 }
