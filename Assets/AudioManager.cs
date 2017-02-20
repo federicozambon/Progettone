a16cb17db1c8fa69f52d407af5b42aaa7529d939
dbga_15 <dbga_15@dbga.local>
2017-02-20 17:23:34 +0100

    menu pausa

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float audiovolume;
+
+
+    public void Update ()
+    {
+        AudioListener.volume = audiovolume;
+    }
+}
