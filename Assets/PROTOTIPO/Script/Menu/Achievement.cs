8a3b6f15575e3ff11af81bdc9a018c4339532f70
Daniele Sangineto <scidros@gmail.com>
2017-03-05 16:42:08 +0100

    - Sistema di salvataggio degli sbloccabili sull'oggetto Achievement (DontDestroyOnLoad) - Comunicazione tra UIController e Achievement - Implementata meccanica di azzeramento di tutti i valori tramite il menu cancella record

--- Assets/PROTOTIPO/Script/Menu/Achievement.cs
+++ Assets/PROTOTIPO/Script/Menu/Achievement.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class Achievement : MonoBehaviour {
+public class Achievement : MonoBehaviour
+{
 
     public int total = 0;
     public int tutorial = 0;
@@ -17,8 +18,29 @@ public class Achievement : MonoBehaviour {
     public string currentScene;
     public bool tutorialComplete = false;
 
+    public int sbloccoMontacarichi = 0;
+    public int achievementMontacarichi_1 = 0;
+    public int achievementMontacarichi_2 = 0;
+    public int achievementMontacarichi_3 = 0;
 
-    void Awake () {
+    public int sbloccoDiscarica = 0;
+    public int achievementDiscarica_1 = 0;
+    public int achievementDiscarica_2 = 0;
+    public int achievementDiscarica_3 = 0;
+
+    public int sbloccoAscensore = 0;
+    public int achievementAscensore_1 = 0;
+    public int achievementAscensore_2 = 0;
+    public int achievementAscensore_3 = 0;
+
+    public int sbloccoTetto = 0;
+    public int achievementTetto_1 = 0;
+    public int achievementTetto_2 = 0;
+    public int achievementTetto_3 = 0;
+
+
+    void Awake()
+    {
 
         DontDestroyOnLoad(this.gameObject);
         muovitiElements = FindObjectOfType<Muoviti>();
@@ -31,20 +53,64 @@ public class Achievement : MonoBehaviour {
             montacarichiB = PlayerPrefs.GetInt("scoreMontacarichiB", montacarichiB);
             tetto = PlayerPrefs.GetInt("scoreTetto", tetto);
 
+            sbloccoMontacarichi = PlayerPrefs.GetInt("sbloccoMontacarichiUI", sbloccoMontacarichi);
+            sbloccoDiscarica = PlayerPrefs.GetInt("sbloccoDiscaricaUI", sbloccoDiscarica);
+            sbloccoAscensore = PlayerPrefs.GetInt("sbloccoAscensoreUI", sbloccoAscensore);
+            sbloccoTetto = PlayerPrefs.GetInt("sbloccoTettoUI", sbloccoTetto);
+
+            achievementMontacarichi_1 = PlayerPrefs.GetInt("achievementMontacarichi_1", achievementMontacarichi_1);
+            achievementMontacarichi_2 = PlayerPrefs.GetInt("achievementMontacarichi_2", achievementMontacarichi_2);
+            achievementMontacarichi_3 = PlayerPrefs.GetInt("achievementMontacarichi_3", achievementMontacarichi_3);
+
+            achievementDiscarica_1 = PlayerPrefs.GetInt("achievementDiscarica_1", achievementDiscarica_1);
+            achievementDiscarica_2 = PlayerPrefs.GetInt("achievementDiscarica_2", achievementDiscarica_2);
+            achievementDiscarica_3 = PlayerPrefs.GetInt("achievementDiscarica_3", achievementDiscarica_3);
+
+            achievementAscensore_1 = PlayerPrefs.GetInt("achievementAscensore_1", achievementAscensore_1);
+            achievementAscensore_2 = PlayerPrefs.GetInt("achievementAscensore_2", achievementAscensore_2);
+            achievementAscensore_3 = PlayerPrefs.GetInt("achievementAscensore_3", achievementAscensore_3);
+
+            achievementTetto_1 = PlayerPrefs.GetInt("achievementTetto_1", achievementTetto_1);
+            achievementTetto_2 = PlayerPrefs.GetInt("achievementTetto_2", achievementTetto_2);
+            achievementTetto_3 = PlayerPrefs.GetInt("achievementTetto_3", achievementTetto_3);
+
             firstLoad = false;
-            
+
             PlayerPrefs.SetInt("scoreTotale", total);
             PlayerPrefs.SetInt("scoreMontacarichiA", montacarichiA);
             PlayerPrefs.SetInt("scoreDiscarica", discarica);
             PlayerPrefs.SetInt("scoreMontacarichiB", montacarichiB);
             PlayerPrefs.SetInt("scoreTetto", tetto);
 
-            PlayerPrefs.Save();
-        }                 
-    }
-   
+            PlayerPrefs.SetInt("sbloccoMontacarichiUI", sbloccoMontacarichi);
+            PlayerPrefs.SetInt("sbloccoDiscaricaUI", sbloccoDiscarica);
+            PlayerPrefs.SetInt("sbloccoAscensoreUI", sbloccoAscensore);
+            PlayerPrefs.SetInt("sbloccoTettoUI", sbloccoTetto);
 
-    public void SaveScore (int score)
+            PlayerPrefs.SetInt("achievementMontacarichi_1", achievementMontacarichi_1);
+            PlayerPrefs.SetInt("achievementMontacarichi_2", achievementMontacarichi_2);
+            PlayerPrefs.SetInt("achievementMontacarichi_3", achievementMontacarichi_3);
+
+            PlayerPrefs.SetInt("achievementDiscarica_1", achievementDiscarica_1);
+            PlayerPrefs.SetInt("achievementDiscarica_2", achievementDiscarica_2);
+            PlayerPrefs.SetInt("achievementDiscarica_3", achievementDiscarica_3);
+
+            PlayerPrefs.SetInt("achievementDiscarica_1", achievementAscensore_1);
+            PlayerPrefs.SetInt("achievementDiscarica_2", achievementAscensore_2);
+            PlayerPrefs.SetInt("achievementDiscarica_3", achievementAscensore_3);
+
+            PlayerPrefs.SetInt("achievementDiscarica_1", achievementTetto_1);
+            PlayerPrefs.SetInt("achievementDiscarica_2", achievementTetto_2);
+            PlayerPrefs.SetInt("achievementDiscarica_3", achievementTetto_3);
+
+            PlayerPrefs.Save();
+        }
+
+
+    }
+
+
+    public void SaveScore(int score)
     {
 
 
@@ -58,7 +124,7 @@ public class Achievement : MonoBehaviour {
                     montacarichiA = score;
                     TotalCount();
                 }
-                
+
                 break;
 
             case "Discarica":
@@ -93,3 +159,4 @@ public class Achievement : MonoBehaviour {
         total = montacarichiA + discarica + montacarichiB + tetto;
     }
 }
+
