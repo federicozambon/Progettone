3c23f609bc73c236c0b49c4cf7f096847f9b40ad
Zambon Federico <zambon.fe@gmail.com>
2017-03-07 03:04:50 +0100

    no message

--- Assets/PROTOTIPO/Script/Enemies/Ranged/TitanoEnemyFire.cs
+++ Assets/PROTOTIPO/Script/Enemies/Ranged/TitanoEnemyFire.cs
@@ -31,11 +31,11 @@ public class TitanoEnemyFire : MonoBehaviour
         id = transform.GetSiblingIndex();
         myParticle = pool.transform.GetChild(id);
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 5; i++)
         {
             transformTr[i] = myParticle.transform.GetChild(i);
-            myEffect[i] = myParticle.transform.GetChild(i + 10).GetComponent<EffectSettings>();
-            myDamage[i] = myEffect[i].transform.FindChild("Trail").GetComponent<bugtitanobullet>();
+            myEffect[i] = myParticle.transform.GetChild(i + 5).GetComponent<EffectSettings>();
+            myDamage[i] = myEffect[i].transform.FindChild("MovedTrail").GetComponent<bugtitanobullet>();
         }
     }
 
@@ -52,38 +52,29 @@ public class TitanoEnemyFire : MonoBehaviour
 
     public IEnumerator Shooting()
     {
+        Debug.Log("ciao");
         isShooting = true;
         if (Physics.Linecast(weapon.transform.position, refManager.playerObj.transform.position, out losRayHit))
         {
             enemyRef.animRef.SetBool("PreAttack", true);
-            yield return new WaitForSeconds(0.35f);
             enemyRef.animRef.SetBool("PreAttack", false);
             enemyRef.animRef.SetBool("Attack", true);
-            for (int i = 0; i < 10; i++)
-            {            
-                if (losRayHit.collider.gameObject.tag == "Player" && Vector3.Distance(refManager.playerObj.transform.position, this.transform.position) < 15)
-                {
-                    ParticleActivator(refManager.playerObj.transform.FindChild("Head").position,i);
-                }
-                yield return new WaitForSeconds(0.35f);
-            }
-            isShooting = false;
-            enemyRef.animRef.SetBool("Attack", false);
-
-            yield return new WaitForSeconds(1f);
-
-            if (GetComponent<TitanoEnemy>().hPoints > 0)
+       
+            if (losRayHit.collider.gameObject.tag == "Player" && Vector3.Distance(refManager.playerObj.transform.position, this.transform.position) < 30)
             {
-                StartCoroutine(Shooting());
+                ParticleActivator(refManager.playerObj.transform.FindChild("Head").position);                   
             }
-            else
-            {
-                StopAllCoroutines();
-            }
-
+        }
+        enemyRef.animRef.SetBool("Attack", false);      
+        yield return new WaitForSeconds(0.3f);
+        isShooting = false;
+        enemyRef.attacking = null;
+        if (GetComponent<TitanoEnemy>().hPoints < 0)
+        {
+            StopAllCoroutines();
         }
     }
-
+   
     public GameObject pool;
     public Transform[] transformTr;
     Transform myParticle;
@@ -91,12 +82,23 @@ public class TitanoEnemyFire : MonoBehaviour
     EffectSettings[] myEffect;
     bugtitanobullet[] myDamage;
 
-    public void ParticleActivator(Vector3 position, int n)
+    int counter = 0; 
+
+    public void ParticleActivator(Vector3 position)
     {
-        transformTr[n].position = position;
-        myDamage[n].damage = (int)enemyRef.damage;
-        myEffect[n].transform.position = weapon.transform.position;
-        myEffect[n].Target = transformTr[n].gameObject;
-        myEffect[n].gameObject.SetActive(true);
+        if (counter<5)
+        {
+            counter++;
+        }
+        else
+        {
+            counter = 0;
+        }
+
+        myDamage[counter].damage = (int)enemyRef.damage;
+        transformTr[counter].position = position;
+        myEffect[counter].transform.position = weapon.transform.position;    
+        myEffect[counter].Target = transformTr[counter].gameObject;
+        myEffect[counter].gameObject.SetActive(true);
     }
 }
